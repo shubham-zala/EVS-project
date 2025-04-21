d690411407b9cc571100cb0199b8647492ebd92f
shubham <shubhamzala545@gmail.com>
2025-04-21 20:08:42 +0530

    Added .env and updated db.js for deployment

--- database/db.js
+++ database/db.js
@@ -1,15 +1,11 @@
+require('dotenv').config();
 const { Pool } = require('pg');
 
-// Create a pool instance
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'EVS',
-    password: 'admin',
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
-// Handle errors during pool creation
 pool.on('error', (err, client) => {
     console.error('Unexpected error on idle client', err);
     process.exit(-1);
